5f8ffe6fa58ed8d14c39f1f8e0cf7565690f5a0a
Graham Melcher <melcher@gmail.com>
2013-06-30 15:36:19 -0700

    deploy master or if there's a pull request

--- script/deploy-heroku.rb
+++ script/deploy-heroku.rb
@@ -1,9 +1,42 @@
 #!/usr/bin/env ruby -w
 require 'heroku-headless'
-pull_request_num = ENV['TRAVIS_PULL_REQUEST']
-production = !pull_request_num || pull_request_num == 'false'
-server_suffix = production ? 'production' : pull_request_num
-HerokuHeadless.configure do | config |
-  config.post_deploy_commands = ['rake db:migrate']
+def pull_request_number
+  ENV['TRAVIS_PULL_REQUEST']
 end
-HerokuHeadless::Deployer.deploy("blue-register-#{suffix}")
+
+def pull_request?
+  pull_request_number && pull_request_number != 'false'
+end
+
+def deployable_branch?
+  !!server_for_branch
+end
+
+def deployable?
+  pull_request? || deployable_branch?
+end
+
+def server_for_branch
+  branch = ENV['TRAVIS_BRANCH']
+  case branch
+  when 'master'
+    'production'
+  else
+    puts "No server configured for #{branch_name}..."
+  end
+end
+
+def derive_server_name(base_name)
+  if pull_request?
+    "#{base_name}-#{pull_request_number}"
+  else
+    "#{base_name}-#{server_for_branch}"
+  end
+end
+
+if deployable?
+  HerokuHeadless.configure do | config |
+    config.post_deploy_commands = ['rake db:migrate']
+  end
+  HerokuHeadless::Deployer.deploy(derive_server_name('blue-register'))
+end
