126238e1a23d6760d1bc5b5e48640845ca83f242
Graham Melcher <melcher@gmail.com>
2013-07-02 18:10:25 -0700

    added round-trip for registries

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,7 +4,13 @@ class ProvidersController < ApplicationController
   end
 
   def create
-
+    @provider = Provider.new(params[:provider])
+    if @provider.create
+      flash[:message] = "Success! Please allow 24 hours for us to approve"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 end
--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -5,6 +5,13 @@ class RegistriesController < ApplicationController
   end
 
   def create
+    @registry = Registry.new(params[:registry])
+    if @registry.save
+      flash[:message] = "Success! Please allow 24 hours for us to approve"
+      redirect_to root_path
+    else
+      render :new
+    end
 
   end
 
