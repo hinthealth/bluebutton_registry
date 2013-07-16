450023c4d63312b530a92e6110c133b6cb7e8629
Graham Melcher <melcher@gmail.com>
2013-07-16 14:10:50 -0700

    Adds new app registration confirmation page

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -6,13 +6,16 @@ class AppsController < ApplicationController
   def create
     @app = App.new(params[:app])
     if @app.save && @app.generate_token!(root_url)
-      AppMailer.registered(@app).deliver
+      # AppMailer.registered(@app).deliver # TODO: Enable outbound email.
       flash[:notice] = "Success! Your app is now listed in apps.json"
-      redirect_to root_path
+      redirect_to app_path(@app.id)
     else
       render :new
     end
   end
 
+  def show
+    @app = App.find(params[:id])
+  end
 
 end
